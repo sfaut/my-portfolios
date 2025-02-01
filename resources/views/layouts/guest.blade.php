e9571b144f06b2132574c1a2b937077b2bc378e9
Sébastien <sebastien.faut@gmail.com>
2025-02-01 18:57:29 +0000

    Review template and CSS

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,15 +1,30 @@
 <x-empty-layout>
 
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div @class([
+        'min-h-screen',
+        'flex', 'flex-col',
+        'items-center', 'justify-center',
+        'bg-gray-100',
+    ])>
+
         <div>
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
+                <x-application-logo class="w-20 h-20 fill-current text-indigo-800"/>
             </a>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        <h1 class="mt-6 text-4xl tracking-widest drop-shadow-md font-medium text-indigo-800">MyPortfolios</h1>
+
+        <div @class([
+            'max-w-md', 'w-full',
+            'mt-6', 'px-6', 'py-6',
+            'bg-white',
+            'shadow-md', 'overflow-hidden',
+            'rounded-lg',
+        ])>
             {{ $slot }}
         </div>
+
     </div>
 
 </x-empty-layout>
