c7d352b46964d8dc0d745ef6812e81719d0b57d2
Sébastien <sebastien.faut@gmail.com>
2025-02-08 14:48:11 +0000

    Initial commit

---
+++ resources/views/pagination/narrow.blade.php
@@ -0,0 +1,97 @@
+{{--
+    Must be call only from paginator instance
+    $records->links('pagination.narrow')
+    https://laravel.com/docs/master/pagination
+--}}
+@if ($paginator->hasPages())
+
+    <nav @class([
+        'grid', 'grid-cols-5',
+        'shadow',
+        '__divide-x',
+        'text-xs', 'text-gray-600',
+    ])>
+
+        {{-- First page --}}
+        <a
+            href="{{ $paginator->url(1) }}"
+            @class([
+                'inline-flex', 'items-center', 'justify-center',
+                'p-2', 'bg-white', 'rounded-l-md',
+            ])
+        >
+            <svg class="size-4" viewBox="0 0 20 20"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+            >
+                <path d="M 16,5 L 11,10 L 16,15 M 9,5 L 4,10 L 9,15"/>
+            </svg>
+        </a>
+
+        {{-- Previous page --}}
+        <a
+            href="{{ $paginator->previousPageUrl() }}"
+            @class([
+                'inline-flex', 'items-center', 'justify-center',
+                'p-2', 'bg-white',
+            ])
+        >
+            <svg class="size-4" viewBox="0 0 20 20"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+            >
+                <path d="M 12,5 L 7,10 L 12,15"/>
+            </svg>
+        </a>
+
+        <div class="inline-flex items-center justify-center bg-white p-2 tracking-widest">
+            P. {{ $paginator->currentPage() }}
+        </div>
+
+        {{-- Next page --}}
+        <a
+            href="{{ $paginator->nextPageUrl() }}"
+            @class([
+                'inline-flex', 'items-center', 'justify-center',
+                'p-2', 'bg-white',
+            ])
+        >
+            <svg class="size-4" viewBox="0 0 20 20"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+            >
+                <path d="M 8,5 L 13,10 L 8,15 "/>
+            </svg>
+        </a>
+
+        <!-- Right double chevron -->
+        <a
+            href="{{ $paginator->url($paginator->lastPage()) }}"
+            @class([
+                'inline-flex', 'items-center', 'justify-center',
+                'rounded-r-md', 'p-2', 'bg-white',
+            ])
+        >
+            <svg class="size-4" viewBox="0 0 20 20"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linejoin="round"
+                stroke-linecap="round"
+            >
+                <path d="M 4,5 L 9,10 L 4,15 M 11,5 L 16,10 L 11,15"/>
+            </svg>
+        </a>
+
+    </nav>
+
+@endif
